16d3f54387c83e718b18885cb02f0175edfa4e30
kianlak <lak.kian.ca@gmail.com>
2023-01-02 13:04:03 -0600

    Refractoring

--- src/project/Data/enums.js
+++ src/project/Data/enums.js
@@ -1,3 +1,6 @@
+/* ENUMS */
+
+//Insertion Modes
 const insertMode = {
   ACROSSANDDOWN: "AD",
   ACROSS: "A",
@@ -5,17 +8,20 @@ const insertMode = {
   NONE: "N"
 };
 
+// Crossword type for black box in center
 const presetModes = {
   NOCENTERBLACKBOX: 0,
   CENTERBLACKBOX: 1
 };
 
+// Crossword patterns for no black box in center 
 const noCenterBlackBoxMode = {
   INTERSECTION: 0,
   ACROSSONLY: 1,
   DOWNONLY: 2
 };
 
+// Crossword patterns for black box in center 
 const centerBlackBoxMode = {
   FILLEDAROUNDCENTER: 0,
   FILLEDACROSS: 1,
